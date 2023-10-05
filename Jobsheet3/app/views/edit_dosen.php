b090f28ca13a5de2888a3f5691d1ad2a825d1961
Yefta Charrand Kusuma Putra <yeftakusuma123@gmail.com>
2023-10-05 11:33:21 +0700

    Create edit_dosen.php

---
+++ Jobsheet3/app/views/edit_dosen.php
@@ -0,0 +1,41 @@
+<?php
+include '../classes/database.php';
+$db=new database();
+?>
+
+    <?php
+    foreach ($db->edit_dosen($_GET['id']) as $d){
+    ?>
+
+<h3>Edit Data Dosen</h3>
+<form action="proses_dosen.php?aksi=update" method="post">
+
+    <table>
+    <tr>
+        <td>NIDN</td>
+        <td>
+        <input type="hidden" name="id" value="<?php echo $d['id']?>">
+        <input type="text" name="nidn" value="<?php echo $d['nidn']?>"></td>
+    </tr>
+
+    <tr>
+        <td>Nama</td>
+        <td><input type="text" name="Nama" value="<?php echo $d['Nama']?>"></td>
+    </tr>
+
+    <tr>
+        <td>Alamat</td>
+        <td>
+        <textarea name="Alamat" cols="30" rows="5"><?php echo $d['Alamat']?></textarea>
+    </td>
+
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type ="submit" value="Simpan"></td>
+    </tr>
+    </table>
+<?php
+}
+?>   
+</form>
