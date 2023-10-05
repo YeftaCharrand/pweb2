b27743e39e2039a041ffb58cfc7d832e3548537d
Yefta Charrand Kusuma Putra <yeftakusuma123@gmail.com>
2023-10-05 11:32:27 +0700

    Create string_manipulation.php

---
+++ jobsheet1/string_manipulation.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Manipulation</title>
+</head>
+<body>
+    <?php
+    // $x = "Selamat belajar PHP!";
+    // // echo $x;
+    // // echo strtoupper($x);
+    // // echo strtolower($x);
+    // echo substr($x, 0, 7);
+
+    $x = "Selamat belajar PHP!";
+        // echo $x;
+        // echo strtoupper($x);
+        // echo strtolower($x);
+        // echo substr ($x, 0, 7 );
+
+        $DATE="02-050-2023";
+        $x = "Selamat belajar PHP!";
+        // echo $x;
+        // echo strtoupper($x);
+        // echo strtolower($x);
+        // echo substr ($x, 0, 7 );   
+
+    ?>
+</body>
+</html>
+
